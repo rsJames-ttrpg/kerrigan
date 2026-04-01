b48a3ce7562237d78e59803e2b0fd583dbbcde59
Claude Drone <drone@kerrigan.local>
2026-04-01 16:05:59 +0000

    test(nydus): add hello world test

--- src/nydus/src/lib.rs
+++ src/nydus/src/lib.rs
@@ -5,3 +5,11 @@ mod types;
 pub use client::NydusClient;
 pub use error::Error;
 pub use types::*;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn hello_world() {
+        assert_eq!("hello", "hello");
+    }
+}
